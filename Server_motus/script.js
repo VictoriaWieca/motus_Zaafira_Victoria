c4c884453db347069311df3b0917243f0a540a5d
VictoriaWieca <wiecaszek.victoria@gmail.com>
2022-10-19 14:51:40 +0200

    test

--- Server_motus/script.js
+++ Server_motus/script.js
@@ -4,6 +4,7 @@ const os = require('os');
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
+const { auth } = require('express-openid-connect');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -14,7 +15,21 @@ var current_number = readFileSync('actuel.txt', 'utf-8').split('\n')[0];
 //console.log(`current number : ${current_number}`)
 var date = new Date();
 
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+const session = require('express-session')
+app.set('trust proxy', 1) // trust first proxy
 
+app.use(session({
+  secret: 's3Cur3',
+  name: 'sessionId',
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    expires: expiryDate
+  },
+  resave: true,
+  saveUninitialized: true
+}))
 
 var user = "Mike O'Serviss";
 var password = "azerty";
@@ -24,7 +39,22 @@ app.use((req,res,next)=>{
   next()
  })
 
- app.use(express.static('www'));
+
+app.use((req,res,next)=>{
+  if(req.session.user){
+    next()
+  }else{
+    res.redirect("www/login.html")
+  }
+}) 
+
+app.use(auth({
+  secret: "secure",
+  authRequired: false,
+}));
+
+
+app.use(express.static('www'));
 
 app.post('/isWin', (req, res) => {
   theWin = req.body;
@@ -47,6 +77,13 @@ app.get('/word', (req, res) => {
 })
 app.get('/port', (req, res) => {
   res.send(`MOTUS APP working on ${os.hostname} port ${port}`)
+})
+
+app.get('/session', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>Session expires : ' + req.session.cookie.expires + '</p>')
+    res.send(`hello ${req.oidc.user.sub}`);
+    res.end()
 })
 app.listen(port, () => {
   console.log(`Motus app listening on port ${port}`)
