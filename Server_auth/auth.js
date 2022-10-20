83742e4ed74071baf9824ef558d308a2ea563c28
VictoriaWieca <wiecaszek.victoria@gmail.com>
2022-10-20 15:20:20 +0200

    login début

--- Server_auth/auth.js
+++ Server_auth/auth.js
@@ -4,8 +4,8 @@ var http = require('http');
 
 const express = require('express')
 const app = express()
+const { auth } = require('express-openid-connect');
 
-var { Issuer, Strategy } = require('openid-client');
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -19,79 +19,38 @@ app.use((req,res,next)=>{
  })
 
 const session = require('express-session')
-const passport = require('passport');
-
-app.set('trust proxy', 1) // trust first proxy
-
 app.use(session({
-  secret: 's3Cur3',
-  name: 'sessionId',
+  secret: 'JN5iLfHJ3Zzze1LB8eTDXSomyvzivzGBE6PAXMXPHfGLcWaFPsfr9D7yuWZcoGT7',
+  name: 'gYAseySuNbKfNyPWySZA02FSthdZCpyB',
   resave: true,
   saveUninitialized: true
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-Issuer.discover('http://localhost:5000')
-  .then(local => {
-    console.log("testttttttttttt");
-    var client = new local.Client({
-      client_id: 'urn:criipto:nodejs:demo:1010',
-      client_secret: 'j9wYVyD3zXZPMo3LTq/xSU/sMu9/shiFKpTHKfqAutM=',
-      redirect_uris: [ 'http://localhost:5000/authorize' ],
-      token_endpoint_auth_method: 'client_secret_post'
-    });
-  
-  
-    // do the rest of setup here
-    passport.use(
-      'oidc',
-      new Strategy({ client }, (tokenSet, userinfo, done) => {
-        return done(null, tokenSet.claims());
-      })
-    );
-    
-    passport.serializeUser(function(user, done) {
-      done(null, user);
-    });
-    passport.deserializeUser(function(user, done) {
-      done(null, user);
-    });
-  
-    // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-      next(createError(404));
-    });
-
-    // error handler
-    app.use(function(err, req, res, next) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-      // render the error page
-      res.status(err.status || 500);
-      res.json({
-        message: err.message,
-        error: err
-      });
-    });
-  });
-
 
 
 app.use(express.static('www'));
 
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'http://localhost:5000/',
+  clientID: 'gYAseySuNbKfNyPWySZA02FSthdZCpyB',
+  issuerBaseURL: 'https://dev-sqarahhvhxa0rob7.us.auth0.com',
+  secret: 'JN5iLfHJ3Zzze1LB8eTDXSomyvzivzGBE6PAXMXPHfGLcWaFPsfr9D7yuWZcoGT7'
+};
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-  res.send('hello');
-})
-
-app.get('/authorize', (req, res, next) => {
-  passport.authenticate('oidc', { acr_values: 'urn:grn:authn:fi:all' })(req, res, next);
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 });
+
+
+/*app.get('/authorize', (req, res, next) => {
+  passport.authenticate('oidc', { acr_values: 'urn:grn:authn:fi:all' })(req, res, next);
+});*/
 
 
 app.get('/port', (req, res) => {
